63bf5907c392134eef86e7655df54d5796f342d4
pierredes <pierredesombre.pro@gmail.com>
2021-10-05 18:50:16 +0200

    Add check on password for login

--- src/main/java/com/example/hotel/api/LoginApiController.java
+++ src/main/java/com/example/hotel/api/LoginApiController.java
@@ -5,6 +5,8 @@ import com.example.hotel.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +19,23 @@ public class LoginApiController {
     @Autowired
     private AdminRepository ar;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping(path = "/", produces = "application/json")
     ResponseEntity<AdminEntity> login(@RequestBody AdminEntity admin) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         try {
-            AdminEntity admin1 = ar.findByUsernameAndPassword(admin.getUsername(), admin.getPassword());
-            return ResponseEntity.ok().body(admin1);
+            AdminEntity admin1 = ar.findByUsername(admin.getUsername());
+            if (encoder.matches(admin.getPassword(), admin1.getPassword())) {
+                return ResponseEntity.ok().body(admin1);
+            } else {
+                throw new Exception("Mauvais mot de passe");
+            }
+
         } catch(Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "e.getMessage()");
         }
     }
 }
